16c963956d56d082bf11e9cf34542da0f36d1086
171547080 <171547080@qq.com>
2022-05-05 10:24:23 +0800

    添加config获取环境配置

--- app/src/config/index.js
+++ app/src/config/index.js
@@ -1,8 +1,13 @@
 import Config from 'react-native-config';
+const DEFAULF_CONFIG = {
+  webAppUrl:'https://127.0.0.1:80'
+}
+
 
 const result = {
   apiUrl: Config.API_URL,
   version: Config.VERSION,
+  webAppUrl: Config.WEB_APP_URL || DEFAULF_CONFIG.webAppUrl
 };
 
 export default result;
