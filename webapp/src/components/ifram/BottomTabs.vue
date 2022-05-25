57fb7e5ee83f4f9d98bc970bb4022189608c10c1
171547080 <171547080@qq.com>
2022-05-25 16:07:24 +0800

    添加svg图标，添加激活图标动画

--- webapp/src/components/ifram/BottomTabs.vue
+++ webapp/src/components/ifram/BottomTabs.vue
@@ -1,7 +1,10 @@
 <template>
   <tabbar v-model="active" @change="onChange">
-    <tabbar-item v-for="(d, i) in tabs" :key="'tab_' + d.name" :icon="d.icon"
-      >{{ d.text }}
+    <tabbar-item v-for="(d, i) in tabs" :key="'tab_' + d.name">
+      {{ d.text }}
+      <template #icon>
+        <img class="svg-icon" :src="d.img">
+      </template>
     </tabbar-item>
   </tabbar>
 </template>
@@ -9,17 +12,34 @@
 import { Tabbar, TabbarItem } from "vant";
 import "vant/es/Tabbar/style/index";
 import "vant/es/tabbar-item/style/index";
-import { ref } from "vue";
+import { ref,onMounted } from "vue";
 import { routerPush } from "../../router/routerUtil";
+import { useRouter,useRoute } from "vue-router";
 
-const active = ref(0);
 
 const tabs = ref([
-  { name: "home", icon: "home-o", path: "/home", text: "首页" },
-  { name: "product", icon: "search", path: "/search", text: "搜索" },
-  { name: "friends", icon: "friends-o", path: "/friends", text: "朋友圈" },
-  { name: "user", icon: "setting-o", path: "/setting", text: "个人设置" },
+  { name: "product", icon: "search", path: "/search", text: "搜索", img: '/img/bottomTabs/shop.svg' },
+  { name: "friends", icon: "friends-o", path: "/friends", text: "中心", img: '/img/bottomTabs/game.svg' },
+  { name: "home", icon: "home-o", path: "/home", text: "首页", img: '/img/bottomTabs/home.svg' },
+  { name: "user", icon: "setting-o", path: "/setting", text: "朋友", img: '/img/bottomTabs/server.svg' },
+   { name: "user", icon: "setting-o", path: "/setting", text: "个人设置", img: '/img/bottomTabs/user.svg' },
 ]);
+
+// 初始化tab
+// router.currentRoute 一直为 path = [/] 的节点
+const router = useRouter();
+const route = useRoute();
+
+// hash模式
+const currentTabName = router.currentRoute.value.name || window.location.hash.substring(2) ||'home'
+let currentTabIndex = 0
+tabs.value.forEach((d,i)=>{
+    if(d.name === currentTabName){
+      currentTabIndex = i
+    }
+})
+
+const active = ref(0);
 
 const onChange = (i) => {
   const current = (tabs.value || {})[i];
@@ -29,4 +49,15 @@ const onChange = (i) => {
 };
 </script>
 
-<style lang="less" scpoed></style>
+<style lang="less" scpoed>
+.van-tabbar-item__icon .svg-icon{
+  height: 1.7rem;
+  transition:height .3s;
+}
+.van-tabbar-item--active .svg-icon{
+  height: 2.5rem;
+}
+.van-tabbar{
+  height: 4.5rem;
+}
+</style>
