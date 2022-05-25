1be87044bf2d65b808e2d5c08181babe86f5979a
171547080 <171547080@qq.com>
2022-05-25 16:10:41 +0800

    添加轮播图与菜单导航组件

---
+++ webapp/src/views/home/adSwipe.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="swipe-bg"> </div>
+    <swipe class="my-swipe" :autoplay="3000" indicator-color="#fa2c19" :touchable="true">
+        <swipe-item v-for="(d, i) in images" :key="d.name">
+            <div class="card sw-item">
+                <img class="ad-img" :src="d.img">
+            </div>
+        </swipe-item>
+
+        <!-- <template #indicator="{ active, total }">
+            <div class="custom-indicator">{{ active + 1 }}/{{ total }}</div>
+        </template> -->
+    </swipe>
+</template>
+<script>
+import { defineComponent, ref } from "vue"
+import { Swipe, SwipeItem } from "vant";
+import "vant/es/Swipe/style/index";
+import "vant/es/swipe-item/style/index";
+
+export default defineComponent({
+    name: "adSwipe",
+    components: {
+        Swipe,
+        SwipeItem
+    },
+
+    setup(props, context) {
+        const images = ref([
+            { path: '', img: "/ad/ad1.jpg", name: "ad1" },
+            { path: '', img: "/ad/ad2.jpg", name: "ad2" },
+            { path: '', img: "/ad/ad3.jpg", name: "ad3" },
+            { path: '', img: "/ad/ad4.jpg", name: "ad4" },
+            { path: '', img: "/ad/ad5.jpg", name: "ad5" },
+            { path: '', img: "/ad/ad6.jpg", name: "ad6" },
+        ]);
+        return {
+            images,
+        };
+    },
+});
+</script>
+<style lang="less" scoped>
+.my-swipe {
+    .sw-item {
+        color: #fff;
+        font-size: 20px;
+        text-align: center;
+        border-radius: 5px;
+        height: 8rem;
+    }
+
+    .ad-img {
+        width: 100%;
+        overflow: hidden;
+        pointer-events: none;
+        border-radius: 5px;
+    }
+
+    ::v-deep(.van-swipe__indicators) {
+        bottom: 3rem;
+    }
+}
+
+.swipe-bg {
+    background-image: linear-gradient(0deg, #f1503b, #c82519 50%);
+    position: absolute;
+    top: 1.5rem;
+    left: -25%;
+    height: 7.25rem;
+    width: 150%;
+    border-bottom-left-radius: 100%;
+    border-bottom-right-radius: 100%;
+}
+
+.custom-indicator {
+    position: absolute;
+    right: 5px;
+    bottom: 5px;
+    padding: 2px 5px;
+    font-size: 12px;
+    background: rgba(0, 0, 0, 0.1);
+}
+
+.card {
+    margin: 1rem;
+    border-radius: 10px;
+}
+</style>
+
---
+++ webapp/src/views/home/menuSwipe.vue
@@ -0,0 +1,85 @@
+<template>
+    <swipe class="my-swipe" indicator-color="#fa2c19" :touchable="true" :loop="false">
+        <swipe-item v-for="(ms, i) in meums" :key="ms.name">
+            <div class="meum-warp">
+                <li class="meum-item" v-for="(d, i) in ms.meum" :key="ms.name + d.name" >
+                    <img class="meum-img" :src="d.img"/>
+                    <p class="meum-text">{{ d.text }}</p>
+                </li>
+            </div>
+        </swipe-item>
+    </swipe>
+</template>
+<script>
+import { defineComponent, ref } from "vue"
+import { Swipe, SwipeItem } from "vant";
+import "vant/es/Swipe/style/index";
+import "vant/es/swipe-item/style/index";
+
+export default defineComponent({
+    name: "adSwipe",
+    components: {
+        Swipe,
+        SwipeItem
+    },
+
+    setup(props, context) {
+        const meums = ref([
+            {
+                name: "meun1", meum: [
+                    { path: '', img: "/menu/1.png", name: "menu1",text:"超市" },
+                    { path: '', img: "/menu/2.png", name: "menu2",text:"数码电器" },
+                    { path: '', img: "/menu/3.png", name: "menu3",text:"服饰" },
+                    { path: '', img: "/menu/4.png", name: "menu4",text:"生鲜" },
+                    { path: '', img: "/menu/5.png", name: "menu5",text:"货物到家" },
+                    { path: '', img: "/menu/6.png", name: "menu6",text:"充值缴费" },
+                    { path: '', img: "/menu/7.png", name: "menu7",text:"物流查询" },
+                    { path: '', img: "/menu/8.png", name: "menu8",text:"领券" },
+                    { path: '', img: "/menu/9.png", name: "menu9",text:"领金贴" },
+                    { path: '', img: "/menu/10.png", name: "menu10",text:"PLUS会员" },
+                ]
+            },
+            {
+                name: "meun2", meum: [
+                    { path: '', img: "/menu/11.png", name: "menu11",text:"国际" },
+                    { path: '', img: "/menu/12.png", name: "menu12",text:"拍卖"},
+                    { path: '', img: "/menu/13.png", name: "menu13",text:"看病" },
+                    { path: '', img: "/menu/14.png", name: "menu14",text:"3C" },
+                    { path: '', img: "/menu/15.png", name: "menu15",text:"沃尔玛" },
+                    { path: '', img: "/menu/16.png", name: "menu16",text:"美妆馆" },
+                    { path: '', img: "/menu/17.png", name: "menu17",text:"旅行" },
+                    { path: '', img: "/menu/18.png", name: "menu18",text:"二手" },
+                    { path: '', img: "/menu/19.png", name: "menu19",text:"潮燃青年" },
+                    { path: '', img: "/menu/20.png", name: "menu20",text:"全部" }
+                ]
+            }
+        ]);
+        return {
+            meums,
+        };
+    },
+});
+</script>
+<style lang="less" scoped>
+.meum-warp{
+    margin: 0 .1rem;
+}
+.meum-item{
+    display:inline-block;
+    text-align: center;
+    height: 5rem;
+    width: 20%;
+
+    .meum-text{
+        margin: 0;
+        font-size: .9rem;
+        font-weight: 500;
+    }
+    .meum-img{
+    display: inline-block;
+    width: 50%;
+}
+}
+
+</style>
+
